9209df693469d548268622c180fecb3af5224271
Oleksandr <cerebrum94@gmail.com>
2022-08-12 14:29:05 +0300

    added routing, book-list & book-list-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './app.css';
+import withBookstoreService from '../hoc/with-bookstore-service';
 
-import Spinner from '../spinner'
+import { Route, Switch } from 'react-router-dom';
+import { HomePage, CartPage } from '../pages';
 
-const App = () => {
+const App = ({ bookstoreService }) => {
+
   return (
-    <div>
-      <Spinner />
-    </div>
+    <Switch>
+      <Route
+        path='/'
+        component={HomePage}
+        exact
+      />
+      <Route
+        path='/cart'
+        component={CartPage}
+      />
+    </Switch>
   );
 };
 
-export default App;
+export default withBookstoreService()(App);
