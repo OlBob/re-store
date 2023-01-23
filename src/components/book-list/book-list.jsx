daf012759d02fd2edb2c7b27af4b435e97bd3b36
Oleksandr <cerebrum94@gmail.com>
2023-01-23 13:28:52 +0200

    BookList split into BookListContainer & BookList components

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -8,7 +8,19 @@ import { compose } from "../../utils";
 import Spinner from "../spinner/spinner";
 import ErrorIndicator from "../error-indicator";
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+    return (
+        <ul className="book-list">
+            {
+                books.map((book) => {
+                    return <li key={book.id}><BookListItem book={book} /></li>
+                })
+            }
+        </ul>
+    )
+}
+
+class BookListContainer extends Component {
 
     componentDidMount() {
         this.props.fetchBooks()
@@ -20,15 +32,7 @@ class BookList extends Component {
         if (loading) return <Spinner />
         if (error) return <ErrorIndicator />
 
-        return (
-            <ul className="book-list">
-                {
-                    books.map((book) => {
-                        return <li key={book.id}><BookListItem book={book} /></li>
-                    })
-                }
-            </ul>
-        )
+        return <BookList books={books} />
     }
 }
 
@@ -49,4 +53,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
     withBookstoreService(),
     connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
